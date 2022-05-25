5a78d99736f7e98f96b514635184504a9ed7e028
Sonicaii <41628956+Sonicaii@users.noreply.github.com>
2022-05-25 19:11:13 +1000

    wa

--- cogs/misc.py
+++ cogs/misc.py
@@ -40,11 +40,11 @@ class Misc(commands.Cog):
 
 	@app_commands.command(name="id", description="Discord ID to time")
 	async def id(self, interaction: discord.Interaction, discord_id: int):
-		await _process_id(interaction, discord_id, f"`{discord_id}` is equivalent to `{{snowflake_time}}` UTC")
+		await self._process_id(interaction, discord_id, f"`{discord_id}` is equivalent to `{{snowflake_time}}` UTC")
 
 	@app_commands.command(name="user", description="Get when user account was made")
 	async def user(self, interaction: discord.Interaction, user: discord.User):
-		await _process_id(interaction, user, f"Account creation of {user.name} with the ID of `{user.id}`\ntranslates to `{{snowflake_time}}` UTC")
+		await self._process_id(interaction, user, f"Account creation of {user.name} with the ID of `{user.id}`\ntranslates to `{{snowflake_time}}` UTC")
 
 	# @app_commands.command(name="channel", description="Get when channel was made")
 	# async def channel(self, interaction: discord.Interaction, Channel: Choice[discord.TextChannel]):
