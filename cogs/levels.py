9c471e3b1257daaae5fec092d49705ec8a361288
Sonicaii <41628956+Sonicaii@users.noreply.github.com>
2022-05-29 02:57:59 +1000

    message author

--- cogs/levels.py
+++ cogs/levels.py
@@ -320,8 +320,8 @@ class Levels(commands.Cog):
 		await self._update(ctx)
 
 	async def _update(self, ctx, startup=False):
-		print(f"Sudo _update: {self.bot.sudo} {ctx.author.id not in self.bot.sudo}")
-		if ctx.author.id not in self.bot.sudo:
+		print(f"Sudo _update: {self.bot.sudo} {ctx.message.author.id not in self.bot.sudo}")
+		if ctx.message.author.id not in self.bot.sudo:
 			return
 
 		copy = self.user_updates.copy()
