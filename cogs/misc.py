bc02e6252cdeb71fdb5e31e36ec19fa63ce3b3cc
Sonicaii <41628956+Sonicaii@users.noreply.github.com>
2022-05-28 15:12:30 +1000

    no str

--- cogs/misc.py
+++ cogs/misc.py
@@ -81,7 +81,7 @@ class Misc(commands.Cog):
 			else:
 				cur.execute("DELETE FROM prefixes WHERE id = %s", (str(ctx.guild.id),))
 				await self.deliver(ctx)("Reset prefix to `,,`")
-		self.bot._prefix_cache_pop(str(ctx.guild.id))
+		self.bot._prefix_cache_pop(ctx.guild.id)
 
 	@commands.command(pass_context=True, name="stop")
 	@commands.is_owner()
