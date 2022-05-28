363e8b4953d3da0655f9e731993c0aa73a307e9f
Sonicaii <41628956+Sonicaii@users.noreply.github.com>
2022-05-28 15:16:53 +1000

    none

--- init.py
+++ init.py
@@ -100,7 +100,7 @@ async def main():
 			bot.cogpr = cogpr
 			bot.deliver = deliver
 			bot._prefix_factory_init = False
-			bot._prefix_cache_pop = lambda i: _server_prefix.cache.pop(i)
+			bot._prefix_cache_pop = lambda i: _server_prefix.cache.pop(i, None)
 
 			for ext in ["cogs."+i for i in [
 					"levels",
