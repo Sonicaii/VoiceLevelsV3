43bb30fd31ef865911773d169b6a94d4782354b5
Sonicaii <41628956+Sonicaii@users.noreply.github.com>
2022-05-28 22:55:09 +1000

    violet

--- cogs/levels.py
+++ cogs/levels.py
@@ -195,16 +195,16 @@ class Levels(commands.Cog):
 		return await interaction.response.send_message(f"{lookup.name} has spent {current_user_time} seconds in voice channels")
 
 	@commands.hybrid_command(name="level", description="Gets the time spent in voice channel of a specified user")
-	async def level(self, ctx: commands.Context, user: Optional[Union[discord.User, str]] = None):
+	async def level(self, ctx: commands.Context, user: Optional[Union[discord.Object, str]] = None):
 		""" returns human ctx text """
 		await self._level(ctx, user)
 
 	@commands.hybrid_command(name="info", description="Gets the time spent in voice channel of a specified user")
-	async def info(self, ctx: commands.Context, user: Optional[Union[discord.User, str]] = None):
+	async def info(self, ctx: commands.Context, user: Optional[Union[discord.Object, str]] = None):
 		await self._level(ctx, user)
 
 	@commands.hybrid_command(name="time", description="Gets the time spent in voice channel of a specified user")
-	async def time(self, ctx: commands.Context, user: Optional[Union[discord.User, str]] = None):
+	async def time(self, ctx: commands.Context, user: Optional[Union[discord.Object, str]] = None):
 		await self._level(ctx, user)
 
 	async def _level(self, ctx, user):
