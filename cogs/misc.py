7ff42d29c66f32acb564942a4d9d8a6521dd1873
Sonicaii <41628956+Sonicaii@users.noreply.github.com>
2022-06-07 12:28:32 +1000

    fixed import

--- cogs/misc.py
+++ cogs/misc.py
@@ -2,6 +2,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 from discord.utils import snowflake_time
+from time import time
 from typing import Union, Optional
 from re import findall
 
@@ -14,7 +15,7 @@ class Misc(commands.Cog):
 	@commands.Cog.listener()
 	async def on_ready(self):
 		self.bot.cogpr("Misc", self.bot)
-		self.start_time = time.time()
+		self.start_time = time()
 
 	async def cog_unload(self):
 		pass
@@ -22,7 +23,7 @@ class Misc(commands.Cog):
 	@commands.command(pass_context=True, name="uptime", description="Get uptime of bot")
 	async def uptime(self, ctx: commands.Context):
 		if ctx.author.id in self.bot.sudo:
-			await self.deliver(ctx)(f"Time since last restart: {time.time() - self.start_time}")
+			await self.deliver(ctx)(f"Time since last restart: {time() - self.start_time}")
 
 	@commands.hybrid_command(name="members", description="Gets the number of members in the server")
 	async def members(self, ctx: commands.Context):
