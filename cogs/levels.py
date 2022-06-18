0f63e55f4bdf64bcb2dd69ef4499fc24c5e19594
Sonicaii <41628956+Sonicaii@users.noreply.github.com>
2022-06-19 04:51:24 +1000

    added space for better accessibility

--- cogs/levels.py
+++ cogs/levels.py
@@ -88,7 +88,7 @@ class Levels(commands.Cog):
 			>>> var = {"id": time, "id": time ... }
 			>>> results = [(str(i).zfill(2), {},) for i in range(100)]
 			>>> for k, v in var.items(): results[int(str(k)[-2:])][1][k] = v
-			>>> conn = psycopg2.connect('YOUR_DATABASE_URL', sslmode='require')
+			>>> conn = psycopg2.connect( 'YOUR_DATABASE_URL', sslmode='require')
 			>>> cur = conn.cursor()
 			>>> # cur.execute( THE COMMAND UNDER THIS COMMENT
 			>>> conn.commit()
