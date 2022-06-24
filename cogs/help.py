1bcd5266fd7facbebbbea048107abc69e53171e4
Sonicaii <41628956+Sonicaii@users.noreply.github.com>
2022-06-24 17:50:16 +1000

    Now gets current message prefix as example

--- cogs/help.py
+++ cogs/help.py
@@ -1,91 +1,107 @@
+"""Help cog"""
+
 import discord
-from discord import app_commands
 from discord.ext import commands
-from typing import Any, Optional, Literal
 
 
-class Help(commands.Cog, name='Help'):
+class Help(commands.Cog):
+    """Help cog contains help command with descriptions of each command in the bot"""
 
-	def __init__(self, bot):
-		self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-	@commands.Cog.listener()
-	async def on_ready(self):
-		self.bot.cogpr("Help", self.bot)
+    @commands.Cog.listener()
+    async def on_ready(self):
+        """Log cog activation"""
+        self.bot.cogpr("Help", self.bot)
 
-	@commands.hybrid_command(name="help", description="Get help about Voice Level's functions!")
-	async def help(self, ctx: commands.Context,
-		# subcommand: Optional[Literal["levels", "snipe",]]
-	):
-		msg = ""
-		embed = discord.Embed(description="_ _\n_ _", colour=0xaeffae)
+    @commands.hybrid_command(
+        name="help", description="Get help about Voice Level's functions!"
+    )
+    async def help(self, ctx: commands.Context):
+        """Generates help command in embed"""
+        message_prefix = (
+            ctx.message.content.split("help")[0]
+            if ctx.message.content else
+            (await self.bot.get_prefix(ctx))[0]
+        )
 
-		desc: list = ["" for i in range(4)]
+        msg = ""
+        embed = discord.Embed(description="_ _\n_ _", colour=0xAEFFAE)
 
-		modules: list = [
-			(
-				"``top     ``",
-				"""This command lists the server's members by voice level rank.
-*``Aliases: leaderboard, all``*""",
-			),
-			(
-				"``total   ``",
-				"""This command gives your total time in seconds, minutes, hours and days.
-*``Aliases: seconds``*""",
-			),
-			(
-				"``level   ``",
-				"""This command gives your or the mentioned person their actual calculated time.
-*``Aliases: time, info``*""",
-			),
-			(
-				"``snipe   ``",
-				"""This command gives you the most recent message that was deleted.
-Putting a number after "snipe" will get you the message that was deleted at the specified distance away.
-*``e.g. snipe 3 will get the message 3 deleted messages ago.``*""",
-			),
-		]
+        desc: list = ["" for i in range(4)]
 
-		misc: list = [
-			(
-				"``members ``",
-				"This command gives you the amount of members of this server.",
-			),
-			(
-				"``ping    ``",
-				"This command gives you the latency of this bot.\n*``Aliases: latency``*",
-			),
-			(
-				"``lookup  ``",
-				"""This command translate Discord snowflake IDs (any Discord ID)
-to their date of creation. Discord IDs are linked to their creation time.
-*``Slash commands: user, channel, id``*""",
-			),
-			("``prefix   ``", """Changes prefix of this bot for this server
-you can set a prefix with spaces in the middle and end by entering:
->    `%sprefix NEW PREFIX    \\`
-the `\\` signals the end of the prefix""" % (await self.bot.get_prefix(ctx))[-1]),
-		]
+        modules: list = [
+            (
+                "``top     ``",
+                "This command lists the server's members by voice level rank."
+                "\n*``Aliases: leaderboard, all``*",
+            ),
+            (
+                "``total   ``",
+                "This command gives your total time in seconds, minutes, hours and days."
+                "\n*``Aliases: seconds``*",
+            ),
+            (
+                "``level   ``",
+                "This command gives your or the mentioned person their actual calculated time."
+                "\n*``Aliases: time, info``*",
+            ),
+            (
+                "``snipe   ``",
+                "This command gives you the most recent message that was deleted.\n"
+                "Putting a number after \"snipe\" will get you the message that was "
+                "deleted at the specified distance away.\n"
+                "*``e.g. snipe 3 will get the message 3 deleted messages ago.``*",
+            ),
+        ]
 
-		if ctx.author.id in self.bot.sudo and not ctx.guild:
-			msg = f'**IMPORTANT**\n**RUN "STOP" TO KILL BOT IN CASE OF EMERGENCY**'
-			embed.add_field(
-				name='**IMPORTANT**',
-				value='**RUN "STOP" TO KILL BOT IN CASE OF EMERGENCY**',
-				inline=False,
-				)
+        misc: list = [
+            (
+                "``members ``",
+                "This command gives you the amount of members of this server.",
+            ),
+            (
+                "``ping    ``",
+                "This command gives you the latency of this bot.\n*``Aliases: latency``*",
+            ),
+            (
+                "``lookup  ``",
+                "This command translate Discord snowflake IDs (any Discord ID)\n"
+                "to their date of creation. Discord IDs are linked to their creation time."
+                "\n*``Slash commands: user, channel, id``*",
+            ),
+            (
+                "``prefix   ``",
+                (
+                    "Changes prefix of this bot for this server\n"
+                    "you can set a prefix with spaces in the middle and end by entering:"
+                    "\n>    `%sprefix NEW PREFIX    \\`"
+                    "\nthe `\\` signals the end of the prefix"
+                ) % message_prefix,
+            ),
+        ]
 
-		for x, y in modules:
-			desc[0] += f'**{x}** - {y}\n\n'
-		for x, y in misc:
-			desc[1] += f'**{x}** - {y}\n\n'
+        if ctx.author.id in self.bot.sudo and not ctx.guild:
+            msg = "**IMPORTANT**\n**RUN \"STOP\" TO KILL BOT IN CASE OF EMERGENCY**"
+            embed.add_field(
+                name="**IMPORTANT**",
+                value="**RUN \"STOP\" TO KILL BOT IN CASE OF EMERGENCY**",
+                inline=False,
+            )
 
-		embed.add_field(name='Commands', value=desc[0])
-		embed.add_field(name='_ _\nMiscellaneous', value=desc[1], inline=False)
-		embed.set_author(name="Help Panel")
+        for name, description in modules:
+            desc[0] += f"**{name}** - {description}\n\n"
+        for name, description in misc:
+            desc[1] += f"**{name}** - {description}\n\n"
 
-		await self.bot.deliver(ctx)(msg, embed=embed)
+        embed.add_field(name="Commands", value=desc[0])
+        embed.add_field(name="_ _\nMiscellaneous", value=desc[1], inline=False)
+        embed.set_author(name="Help Panel")
+
+        await self.bot.deliver(ctx)(msg, embed=embed)
 
 
 async def setup(bot):
-	await bot.add_cog(Help(bot))
+    """Setup"""
+    await bot.add_cog(Help(bot))
