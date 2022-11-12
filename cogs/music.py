9d08199a05adf560c1f734ce50470ad118f8dce0
Sonicaii <41628956+Sonicaii@users.noreply.github.com>
2022-11-13 00:18:42 +1100

    refractor as fstrings

--- cogs/music.py
+++ cogs/music.py
@@ -99,7 +99,7 @@ class YTDLSource(discord.PCMVolumeTransformer):  # pylint: disable=too-many-inst
         self.played = False
 
     def __str__(self):
-        return '**{0.title}** by **{0.uploader}**'.format(self)
+        return f'**{self.title}** by **{self.uploader}**'
 
     @classmethod
     async def create_source(cls, author, channel, search, *, loop: asyncio.BaseEventLoop = None):
@@ -110,7 +110,7 @@ class YTDLSource(discord.PCMVolumeTransformer):  # pylint: disable=too-many-inst
         data = await loop.run_in_executor(None, partial)
 
         if data is None:
-            raise YTDLError('Couldn\'t find anything that matches `{}`'.format(search))
+            raise YTDLError(f'Couldn\'t find anything that matches `{search}`')
 
         if 'entries' not in data:
             process_info = data
@@ -122,14 +122,14 @@ class YTDLSource(discord.PCMVolumeTransformer):  # pylint: disable=too-many-inst
                     break
 
             if process_info is None:
-                raise YTDLError('Couldn\'t find anything that matches `{}`'.format(search))
+                raise YTDLError(f'Couldn\'t find anything that matches `{search}`')
 
         webpage_url = process_info['webpage_url']
         partial = functools.partial(cls.ytdl.extract_info, webpage_url, download=False)
         processed_info = await loop.run_in_executor(None, partial)
 
         if processed_info is None:
-            raise YTDLError('Couldn\'t fetch `{}`'.format(webpage_url))
+            raise YTDLError(f'Couldn\'t fetch `{webpage_url}`')
 
         if 'entries' not in processed_info:
             info = processed_info
@@ -138,8 +138,8 @@ class YTDLSource(discord.PCMVolumeTransformer):  # pylint: disable=too-many-inst
             while info is None:
                 try:
                     info = processed_info['entries'].pop(0)
-                except IndexError:
-                    raise YTDLError('Couldn\'t retrieve any matches for `{}`'.format(webpage_url))
+                except IndexError as exc:
+                    raise YTDLError(f'Couldn\'t retrieve any matches for `{webpage_url}`') from exc
 
         return cls(
             author,
@@ -161,13 +161,13 @@ class YTDLSource(discord.PCMVolumeTransformer):  # pylint: disable=too-many-inst
 
         duration = []
         if days > 0:
-            duration.append('{} days'.format(days))
+            duration.append(f'{days} days')
         if hours > 0:
-            duration.append('{} hours'.format(hours))
+            duration.append(f'{hours} hours')
         if minutes > 0:
-            duration.append('{} minutes'.format(minutes))
+            duration.append(f'{minutes} minutes')
         if seconds > 0:
-            duration.append('{} seconds'.format(seconds))
+            duration.append(f'{seconds} seconds')
 
         return ', '.join(duration)
 
@@ -183,15 +183,15 @@ class Song:  # pylint: disable=too-few-public-methods
     def create_embed(self):
         """Creates an embed"""
         embed = (discord.Embed(title='Now playing',
-                               description='```css\n{0.source.title}\n```'.format(self),
+                               description=f'```css\n{self.source.title}\n```',
                                color=discord.Color.blurple())
                  .add_field(name='Duration', value=self.source.duration)
                  .add_field(name='Requested by', value=self.requester.mention)
                  .add_field(
                      name='Uploader',
-                     value='[{0.source.uploader}]({0.source.uploader_url})'.format(self)
+                     value=f'[{self.source.uploader}]({self.source.uploader_url})'
                  )
-                 .add_field(name='URL', value='[Click]({0.source.url})'.format(self))
+                 .add_field(name='URL', value=f'[Click]({self.source.url})')
                  .set_thumbnail(url=self.source.thumbnail))
 
         return embed
@@ -384,7 +384,7 @@ class Music(commands.Cog):
         ctx.voice_state = self.get_voice_state(ctx)
 
     async def cog_command_error(self, ctx: commands.Context, error: commands.CommandError):
-        await ctx.send('An error occurred: {}'.format(str(error)))
+        await ctx.send(f'An error occurred: {error}')
 
     @commands.command(name='join', invoke_without_subcommand=True)
     async def _join(self, ctx: commands.Context):
@@ -436,7 +436,7 @@ class Music(commands.Cog):
             return await ctx.send('Volume must be between 0 and 100')
 
         ctx.voice_state.volume = volume / 100
-        await ctx.send('Volume of the player set to {}%'.format(volume))
+        await ctx.send(f'Volume of the player set to {volume}%'))
 
     @commands.command(name='now', aliases=['current', 'playing', 'np'])
     async def _now(self, ctx: commands.Context):
@@ -501,11 +501,11 @@ class Music(commands.Cog):
 
         queue = ''
         for i, song in enumerate(ctx.voice_state.songs[start:end], start=start):
-            queue += '`{0}.` [**{1.source.title}**]({1.source.url})\n'.format(i + 1, song)
+            queue += f'`{i+1}.` [**{song.source.title}**]({song.source.url})\n'
 
         embed = discord.Embed(
-            description='**{} tracks:**\n\n{}'.format(len(ctx.voice_state.songs), queue)
-        ).set_footer(text='Viewing page {}/{}'.format(page, pages))
+            description=f'**{len(ctx.voice_state.songs)} tracks:**\n\n{queue}'
+        ).set_footer(text=f'Viewing page {page}/{pages}')
         await ctx.send(embed=embed)
 
     @commands.command(name='shuffle')
@@ -585,7 +585,7 @@ class Music(commands.Cog):
                 song = Song(source)
 
                 await ctx.voice_state.songs.put(song)
-                await ctx.send('Enqueued {}'.format(str(source)))
+                await ctx.send(f'Enqueued {source}')
 
     @_join.before_invoke
     @_play.before_invoke
